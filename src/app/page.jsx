9f617830ce4ac37cea35457a6312452dfd6d35f7
Kartikey <decten.kartz@gmail.com>
2025-10-27 14:22:26 +0530

    added logo and logo heading

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -22,14 +22,20 @@ function trgrTheme(){
   return (
     <main className="mainWrapper">
        <header className={`headerWrapper ${(theme.stat==0)?"darkTheme":"lightTheme"}`}>
+
              <img  src={`${(theme.stat==0)?"/darkModeIcon.png":"/lightModeIcon.png"}`} className="h-5" onClick={trgrTheme}/>
-             <h1 className="text-2xl  w-full text-center">Personal Finance Tracker</h1>
+             <div className="flex">
+                  <h1 className="text-2xl px-10  w-full text-center">Personal Finance Tracker</h1>
+             </div>
              <Navigation/>
        </header>
        <section className={`pages ${(theme.stat==0)?"lightTheme":"darkTheme"}`}>
-               <div className={`flex items-center w-full box-border border-2  ${(theme.stat==0)?"lightTheme":"darkTheme"}`}>
-                  <video src={(theme.stat==0)?"/pftLightThemeLogo.mp4":"/pftDarkThemeLogo.mp4"}  autoPlay muted loop className="h-100" />
-                  <h1 className="w-full text-center"></h1>
+               <div className={`flex flex-col items-center justify-center w-full h-full py-20  box-border  ${(theme.stat==0)?"lightTheme":"darkTheme"}`}>
+                   <img src={(theme.stat==0)?"/pftLightThemeLogo.png":"/pftDarkThemeLogo.png"} className="h-100 " />
+                   <div className="flex items-center">
+                     <h1 className="w-full border-t-2 p-1 border-b-red-500  box-bordertext-center  text-nowrap text-3xl font-bold">{`All Your Expenses Gets`}</h1>
+                     <h1 className="w-full border-t-2 p-1 border-b-red-500  box-bordertext-center  text-nowrap text-3xl font-bold mb-20 mx-2">{ `𖦏 Tracked Here`}</h1> 
+                   </div>      
                </div>
        </section>
        <section className={`pages ${(theme.stat==0)?"lightTheme":"darkTheme"}`}>
